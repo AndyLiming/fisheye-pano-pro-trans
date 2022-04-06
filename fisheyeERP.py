4d503ac4409383bc19a70f91066d5166266a1361
AndyLiming <mingli6250@163.com>
2022-04-06 22:11:45 +0800

    comment imgs saving in fisheyeErp

--- fisheyeERP.py
+++ fisheyeERP.py
@@ -12,17 +12,18 @@ class ERP2Fisheye:
     self.FovTh = self.FoV / 180 * np.pi
     fish_x, fish_y = np.meshgrid(range(fish_w), range(fish_h))
     fish_x = (fish_x.astype(np.float32) - (fish_w - 1) / 2)
-    fish_y = (fish_y.astype(np.float32) - (fish_h - 1) / 2)
+    #fish_y = (fish_y.astype(np.float32) - (fish_h - 1) / 2)
+    fish_y = (fish_y.astype(np.float32)) * fish_h / (fish_h - 1) - self.radius
     fish_theta = np.sqrt(fish_x * fish_x + fish_y * fish_y) / self.radius * self.FoV  #theta deg
     fish_theta = fish_theta / 180 * np.pi
     fish_phi = np.arctan2(fish_y, fish_x)
     #fish_phi[fish_phi < -np.pi] += 2 * np.pi
     # fish_phi = np.expand_dims(fish_phi, 2)
     # fish_theta = np.expand_dims(fish_theta, 2)
-    fish_phi_save = 255 * (fish_phi - np.min(fish_phi)) / (np.max(fish_phi) - np.min(fish_phi))
-    cv2.imwrite('fish_phi.png', fish_phi_save.astype(np.uint8))
-    fish_theta_save = 255 * (fish_theta - np.min(fish_theta)) / (np.max(fish_theta) - np.min(fish_theta))
-    cv2.imwrite('fishe_theta.png', fish_theta_save.astype(np.uint8))
+    # fish_phi_save = 255 * (fish_phi - np.min(fish_phi)) / (np.max(fish_phi) - np.min(fish_phi))
+    # cv2.imwrite('fish_phi.png', fish_phi_save.astype(np.uint8))
+    # fish_theta_save = 255 * (fish_theta - np.min(fish_theta)) / (np.max(fish_theta) - np.min(fish_theta))
+    # cv2.imwrite('fishe_theta.png', fish_theta_save.astype(np.uint8))
 
     self.invalidMask = (fish_theta > self.FovTh)
     fish_theta[self.invalidMask] = 0
@@ -37,10 +38,10 @@ class ERP2Fisheye:
     theta = np.arccos(coor3d_r[:, :, 1]).astype(np.float32)
     phi = np.arctan2(coor3d_r[:, :, 0], coor3d_r[:, :, 2]).astype(np.float32)
 
-    phi_save = 255 * (phi - np.min(phi)) / (np.max(phi) - np.min(phi))
-    cv2.imwrite('phi.png', phi_save.astype(np.uint8))
-    theta_save = 255 * (theta - np.min(theta)) / (np.max(theta) - np.min(theta))
-    cv2.imwrite('theta.png', theta_save.astype(np.uint8))
+    # phi_save = 255 * (phi - np.min(phi)) / (np.max(phi) - np.min(phi))
+    # cv2.imwrite('phi.png', phi_save.astype(np.uint8))
+    # theta_save = 255 * (theta - np.min(theta)) / (np.max(theta) - np.min(theta))
+    # cv2.imwrite('theta.png', theta_save.astype(np.uint8))
 
     theta = (theta - 0) / (np.pi - 0) * 2 - 1.0
     phi = (phi + np.pi) / (2 * np.pi) * 2 - 1.0
@@ -65,6 +66,7 @@ class Fisheye2ERP:
     self.FovTh = self.FoV / 180 * np.pi
     erp_x, erp_y = np.meshgrid(range(erp_w), range(erp_h))
     phi = (erp_x).astype(np.float32) / (erp_w - 1) * 2 * np.pi - np.pi
+    #phi = ((erp_x).astype(np.float32) - (erp_w - 1) / 2) / (erp_w / 2) * np.pi
     theta = (erp_y).astype(np.float32) / (erp_h - 1) * np.pi
     y = np.cos(theta)
     x = np.sin(theta) * np.sin(phi)
@@ -76,11 +78,11 @@ class Fisheye2ERP:
     fish_phi = np.arctan2(coor3d_r[:, :, 1], coor3d_r[:, :, 0]).astype(np.float32)
     self.invalidMask = (fish_theta > self.FovTh)
 
-    cv2.imwrite('mask.png', self.invalidMask.astype(np.uint8) * 255)
-    fish_phi_save = 255 * (fish_phi - np.min(fish_phi)) / (np.max(fish_phi) - np.min(fish_phi))
-    cv2.imwrite('fish_phi.png', fish_phi_save.astype(np.uint8))
-    fish_theta_save = 255 * (fish_theta - np.min(fish_theta)) / (np.max(fish_theta) - np.min(fish_theta))
-    cv2.imwrite('fishe_theta.png', fish_theta_save.astype(np.uint8))
+    # cv2.imwrite('mask.png', self.invalidMask.astype(np.uint8) * 255)
+    # fish_phi_save = 255 * (fish_phi - np.min(fish_phi)) / (np.max(fish_phi) - np.min(fish_phi))
+    # cv2.imwrite('fish_phi.png', fish_phi_save.astype(np.uint8))
+    # fish_theta_save = 255 * (fish_theta - np.min(fish_theta)) / (np.max(fish_theta) - np.min(fish_theta))
+    # cv2.imwrite('fishe_theta.png', fish_theta_save.astype(np.uint8))
     print(np.max(fish_phi), np.min(fish_phi))
 
     fish_theta[self.invalidMask] = 0.0
@@ -124,13 +126,13 @@ if __name__ == '__main__':
   FoV = 210
   e2f = ERP2Fisheye(1024, 1024, 512, 1024, FoV, rotate)
   f2e = Fisheye2ERP(1024, 1024, 512, 1024, FoV, rotate_r)
-  erp = cv2.imread('../erp.png').transpose((2, 0, 1)).astype(np.float32)
+  erp = cv2.imread('./imgs/erp_ori.png').transpose((2, 0, 1)).astype(np.float32)
   fisheye = e2f.trans(erp)
   erp_2 = f2e.trans(fisheye)
   fisheye = fisheye.transpose((1, 2, 0))
   fisheye = (fisheye - np.min(fisheye)) / (np.max(fisheye) - np.min(fisheye)) * 255
-  cv2.imwrite('fish' + str(FoV) + '.png', fisheye.astype(np.uint8))
+  cv2.imwrite('./imgs/fish' + str(FoV) + '.png', fisheye.astype(np.uint8))
 
   erp_2 = erp_2.transpose((1, 2, 0))
   erp_2 = (erp_2 - np.min(erp_2)) / (np.max(erp_2) - np.min(erp_2)) * 255
-  cv2.imwrite('erp_2_' + str(FoV) + '.png', erp_2.astype(np.uint8))
+  cv2.imwrite('./imgs/erp_2_' + str(FoV) + '.png', erp_2.astype(np.uint8))
